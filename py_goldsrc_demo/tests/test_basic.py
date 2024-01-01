6afe9434d543781292caaaca5a2c364dabd8461f
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2024-01-01 15:02:52 +0300

    Move tests to the package

--- py_goldsrc_demo/tests/test_basic.py
+++ py_goldsrc_demo/tests/test_basic.py
@@ -5,5 +5,5 @@ from py_goldsrc_demo.parse_demo import parse_demo
 
 
 def test_smoke():
-    with open(os.path.abspath('demos/bootcamp_test_short.dem'), 'rb') as f:
+    with open('demos/bootcamp_test_short.dem', 'rb') as f:
         parse_demo(BytesIO(f.read()))
